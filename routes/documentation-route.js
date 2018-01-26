b39cb50a69b82e1d7962c8aebb793ac00200f5f3
smachs <fredgamesb2r@gmail.com>
2018-01-25 22:39:44 -0200

    Updated routes of documentation-route.js

--- routes/documentation-route.js
+++ routes/documentation-route.js
@@ -1,91 +1,91 @@
 const documentationRoutes = require('express').Router();
 
 documentationRoutes.get('/alerts', (req, res) => {
-    res.render('documentation/alerts');
+    res.render('documentation/ui/alerts');
 });
 
 documentationRoutes.get('/animations', (req, res) => {
-    res.render('documentation/animations');
+    res.render('documentation/ui/animations');
 });
 
 documentationRoutes.get('/badges', (req, res) => {
-    res.render('documentation/badges');
+    res.render('documentation/ui/badges');
 });
 
 documentationRoutes.get('/breadcrumbs', (req, res) => {
-    res.render('documentation/breadcrumbs');
+    res.render('documentation/ui/breadcrumbs');
 });
 
 documentationRoutes.get('/buttons', (req, res) => {
-    res.render('documentation/buttons');
+    res.render('documentation/ui/buttons');
 });
 
 documentationRoutes.get('/collapse', (req, res) => {
-    res.render('documentation/collapse');
+    res.render('documentation/ui/collapse');
 });
 
 documentationRoutes.get('/colors', (req, res) => {
-    res.render('documentation/colors');
+    res.render('documentation/ui/colors');
 });
 
 documentationRoutes.get('/dialogs', (req, res) => {
-    res.render('documentation/dialogs');
+    res.render('documentation/ui/dialogs');
 });
 
 documentationRoutes.get('/icons', (req, res) => {
-    res.render('documentation/icons');
+    res.render('documentation/ui/icons');
 });
 
 documentationRoutes.get('/labels', (req, res) => {
-    res.render('documentation/labels');
+    res.render('documentation/ui/labels');
 });
 
 documentationRoutes.get('/list-group', (req, res) => {
-    res.render('documentation/list-group');
+    res.render('documentation/ui/list-group');
 });
 
 documentationRoutes.get('/media-object', (req, res) => {
-    res.render('documentation/media-object');
+    res.render('documentation/ui/media-object');
 });
 
 documentationRoutes.get('/modals', (req, res) => {
-    res.render('documentation/modals');
+    res.render('documentation/ui/modals');
 });
 
 documentationRoutes.get('/notifications', (req, res) => {
-    res.render('documentation/notifications');
+    res.render('documentation/ui/notifications');
 });
 
 documentationRoutes.get('/pagination', (req, res) => {
-    res.render('documentation/pagination');
+    res.render('documentation/ui/pagination');
 });
 
 documentationRoutes.get('/pre-loaders', (req, res) => {
-    res.render('documentation/pre-loaders');
+    res.render('documentation/ui/pre-loaders');
 });
 
 documentationRoutes.get('/progress-bars', (req, res) => {
-    res.render('documentation/progress-bars');
+    res.render('documentation/ui/progress-bars');
 });
 
 documentationRoutes.get('/range-sliders', (req, res) => {
-    res.render('documentation/range-sliders');
+    res.render('documentation/ui/range-sliders');
 });
 
 documentationRoutes.get('/sortable-nestable', (req, res) => {
-    res.render('documentation/sortable-nestable');
+    res.render('documentation/ui/sortable-nestable');
 });
 
 documentationRoutes.get('/tabs', (req, res) => {
-    res.render('documentation/tabs');
+    res.render('documentation/ui/tabs');
 });
 
 documentationRoutes.get('/thumbnails', (req, res) => {
-    res.render('documentation/thumbnails');
+    res.render('documentation/ui/thumbnails');
 });
 
 documentationRoutes.get('/tooltips-popovers', (req, res) => {
-    res.render('documentation/tooltips-popovers');
+    res.render('documentation/ui/tooltips-popovers');
 });
 
 documentationRoutes.get('/typography', (req, res) => {
@@ -93,7 +93,7 @@ documentationRoutes.get('/typography', (req, res) => {
 });
 
 documentationRoutes.get('/waves', (req, res) => {
-    res.render('documentation/waves');
+    res.render('documentation/ui/waves');
 });
 
 documentationRoutes.get('/helper-classes', (req, res) => {
