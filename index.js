a2c4608ecc86a7bee8844d9aa20b53c4594256d3
smachs <fredgamesb2r@gmail.com>
2018-01-27 07:56:31 -0200

    Updated index.js

--- index.js
+++ index.js
@@ -2,44 +2,21 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
-var bodyParser = require('body-parser');
-
-var app = module.exports = loopback();
-
 const documentationRoutes = require('./routes/documentation-route');
 const pageRoutes = require('./routes/pages-route');
 const authRoutes = require('./routes/authentication-route');
 
-// configure view handler
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// configure body parser
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(loopback.token());
-
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
-
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
-
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
-});
+express()
+.use(express.static(path.join(__dirname, 'public')),)
+.use('/', documentationRoutes)
+.use('/', pageRoutes)
+.use('/', authRoutes)
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'ejs')
+.get('/', (req, res) => res.render('pages/index'))
+.listen(PORT, () => console.log(`
+**************************************************************************************
+                        FAMAGÁS ERP 5.6.2 STARTED SUCCESSFULLY           
+                    Check your workstation in: localhost:${ PORT }
+                        http://www.documentation.famagas.com.br
+**************************************************************************************`))
